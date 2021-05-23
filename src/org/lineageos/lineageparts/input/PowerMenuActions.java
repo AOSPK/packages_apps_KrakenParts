LineageOS/android_packages_apps_LineageParts

PowerMenuActions: Add emergency option
This allows toggling Emergency button in GlobalAction(Power menu).

Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
Change-Id: I190048e213a08a969e10aef4081650875dab2587

src/org/lineageos/lineageparts/input/PowerMenuActions.java
@@ -49,6 +49,7 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     private CheckBoxPreference mUsersPref;
     private CheckBoxPreference mBugReportPref;
     private CheckBoxPreference mLockDownPref;
+    private CheckBoxPreference mEmergencyPref;
 
     private GlobalActionsManager mManager;
 
@@ -78,6 +79,8 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
                 mBugReportPref = findPreference(GLOBAL_ACTION_KEY_BUGREPORT);
             } else if (action.equals(GLOBAL_ACTION_KEY_LOCKDOWN)) {
                 mLockDownPref = findPreference(GLOBAL_ACTION_KEY_LOCKDOWN);
+            } else if (action.equals(GLOBAL_ACTION_KEY_EMERGENCY)) {
+                mEmergencyPref = findPreference(GLOBAL_ACTION_KEY_EMERGENCY);
             }
         }
 
@@ -110,6 +113,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
 
         if (mBugReportPref != null) {
             mBugReportPref.setChecked(mManager.userConfigContains(GLOBAL_ACTION_KEY_BUGREPORT));
+        }
+
+        if (mEmergencyPref != null) {
+            mEmergencyPref.setChecked(mManager.userConfigContains(GLOBAL_ACTION_KEY_EMERGENCY));
         }
 
         updatePreferences();
@@ -149,7 +156,12 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
             Settings.Secure.putInt(getContentResolver(),
                     Settings.Secure.LOCKDOWN_IN_POWER_MENU, value ? 1 : 0);
 
-        } else {
+        } else if (preference == mEmergencyPref) {
+            value = mEmergencyPref.isChecked();
+            mManager.updateUserConfig(value, GLOBAL_ACTION_KEY_EMERGENCY);
+        }
+
+        else {
             return super.onPreferenceTreeClick(preference);
         }
         return true;

AOSPK/packages_apps_KrakenParts

src/org/lineageos/lineageparts/input/PowerMenuActions.java
@@ -49,6 +49,7 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     private CheckBoxPreference mUsersPref;
     private CheckBoxPreference mBugReportPref;
     private CheckBoxPreference mLockDownPref;
+    private CheckBoxPreference mEmergencyPref;
 
     private LineageGlobalActions mLineageGlobalActions;
 
@@ -78,6 +79,8 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
                 mBugReportPref = findPreference(GLOBAL_ACTION_KEY_BUGREPORT);
             } else if (action.equals(GLOBAL_ACTION_KEY_LOCKDOWN)) {
                 mLockDownPref = findPreference(GLOBAL_ACTION_KEY_LOCKDOWN);
+            } else if (action.equals(GLOBAL_ACTION_KEY_EMERGENCY)) {
+                mEmergencyPref = findPreference(GLOBAL_ACTION_KEY_EMERGENCY);
             }
         }
 
@@ -116,6 +119,11 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
                     GLOBAL_ACTION_KEY_BUGREPORT));
         }
 
+        if (mEmergencyPref != null) {
+            mEmergencyPref.setChecked(mLineageGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_EMERGENCY));
+        }
+
         updatePreferences();
     }
 
@@ -152,6 +160,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
             mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_LOCKDOWN);
             Settings.Secure.putIntForUser(getContentResolver(),
                     Settings.Secure.LOCKDOWN_IN_POWER_MENU, value ? 1 : 0, UserHandle.USER_CURRENT);
+
+        } else if (preference == mEmergencyPref) {
+            value = mEmergencyPref.isChecked();
+            mLineageGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_EMERGENCY);
 
         } else {
             return super.onPreferenceTreeClick(preference);